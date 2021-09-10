126e956ac2919dba742c99a82cfbfb4bd2338e26
nadyacherepaha <nad.cherepaha@gmail.com>
2021-09-10 13:41:18 +0300

    Add project

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#E0BBE4', '#957DAD', '#D291BC', '#FEC8D8', '#FFDFD3']
+const SQUARES_NUMBER = 1230
+
+for (let i = 0; i < SQUARES_NUMBER; i ++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
